7bd624040f834d14b22d5af291869e5a9eb8a5f9
Pablo <pablocm1998@gmail.com>
2019-11-29 14:07:18 -0600

    funciones.py file update -P

--- funciones.py
+++ funciones.py
@@ -1,8 +1,7 @@
 from astropy.modeling.blackbody import blackbody_lambda
 from scipy.interpolate import interp1d
-# from astropy import units as u
 
-kb  = 1.38e-16 # [ergK-1] 
+kb  = 1.38e-16 # [ergK-1]
 c = 3e10 #cm/s
 
 # Temperature Model [K]
@@ -37,16 +36,14 @@ def n(x):
 	
 # Source function [erg/cm2 sec cm ster]
 def S(x, wl):
-	# return blackbody_lambda(wl*1e8,  T(x)) * 1e8 # A-1 to cm-1
-	return blackbody_lambda(wl*1e8,  T(x)) # A-1 to cm-1
+	return blackbody_lambda(wl*1e8,  T(x)) * 1e8 # A-1 to cm-1
 
 # opacity [cm-1]
 def k(x, wl):
     # REF Dulk (1985) eq. 21
-    # 0.8 - 12.5 mu []
     nu = c/wl
     return 1e5 * 0.2 * pow(n(x),2) * pow(T(x),-3/2) * pow(nu ,-2)
-    # return 0.8
+
 # adimensional - optical depth
 def tau(dx, x, wl):
 	return (dx/2.0)*(k(x-dx, wl)+k(x, wl))
